6be09b949739c2f4ec2f1b2ed1ce6bccab340aeb
Kidwick <115428041+Kidwick@users.noreply.github.com>
2022-11-10 19:18:09 +0300

    Add files via upload

---
+++ Castingexample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    //Variable initialisation
+    float myheight=10.123;
+    int myNum;
+
+    myNum=static_cast<int>(myheight);//float to integer
+
+    return 0;
+}
